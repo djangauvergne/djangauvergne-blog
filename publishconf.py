0fbcb749924f5282c04d7d54ae4c0607dc2a614f
Julien Maupetit <julien@maupetit.net>
2012-11-05 12:29:10 +0100

    Added GA and Disqus production IDs

--- publishconf.py
+++ publishconf.py
@@ -14,5 +14,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # Uncomment following line for absolute URLs in production:
 #RELATIVE_URLS = False
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "djangauvergne"
+GOOGLE_ANALYTICS = "UA-36095581-1"
